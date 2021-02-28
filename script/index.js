d3415943b2bfbfb847eac184a713d2bc522fc4dd
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-02-28 22:24:18 +0900

    add burger

---
+++ script/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    })
+})
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#close').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    })
+
+
+})
