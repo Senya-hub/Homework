7e0a6c3d90dd55498ae189080088e77d91a9e642
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-03-04 23:52:17 +0900

    js

--- script/index.js
+++ script/index.js
@@ -1,12 +1,16 @@
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#burger').addEventListener('click', function() {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-})
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#close').addEventListener('click', function() {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-
-
-})
+$('#burger').click(function(){
+    $('#menu').toggle('.is-active');
+});
+$('#close').click(function(){
+    $('#menu').toggle('.is-active');
+});
+// window.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('#burger').addEventListener('click', function() {
+//         document.querySelector('#menu').classList.toggle('is-active')
+//     })
+// })
+// window.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('#close').addEventListener('click', function() {
+//         document.querySelector('#menu').classList.toggle('is-active')
+//     })
+// })
