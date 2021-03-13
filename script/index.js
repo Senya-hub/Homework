5b4cc289e3cb57cda961a97be4ef026a6f633e2c
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-03-13 20:13:27 +0900

    add finish files

--- script/index.js
+++ script/index.js
@@ -45,4 +45,28 @@ $(function(){
     });
 });
 
+let selector = document.querySelector("input[type='tel']");
+let im = new Inputmask("+7 (999) 999-99-99");
+im.mask(selector);
+
+new JustValidate('.form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 2,
+        maxLength: 30
+      },
+      tel: {
+        required: true,
+        function: (name, value) => {
+          const phone = selector.inputmask.unmaskedvalue()
+          return Number(phone) && phone.length === 10
+        }
+      },
+      mail: {
+        required: true,
+        email: true
+      },
+    },
+  });
         
